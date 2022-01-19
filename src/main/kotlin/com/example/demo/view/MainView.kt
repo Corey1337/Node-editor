3bf19d2dced8ec3b74444fd6fc5257d167be4625
Corey1337 <kolikendr@gmail.com>
2022-01-19 14:58:03 +1000

    init

---
+++ src/main/kotlin/com/example/demo/view/MainView.kt
@@ -0,0 +1,9 @@
+package com.example.demo.view
+
+import com.example.demo.app.Styles
+import javafx.scene.layout.VBox
+import tornadofx.*
+
+class MainView : View("Node IMG Editor") {
+    override val root: VBox by fxml("/view.fxml");
+}
